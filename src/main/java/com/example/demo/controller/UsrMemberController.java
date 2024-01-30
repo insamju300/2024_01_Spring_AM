c089aa36960d08730f479f1629f97d139421f295
insamju300 <insamju300@gmail.com>
2024-01-30 14:43:02 +0900

    isNullOrEmpty

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.MemberService;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 
 @Controller
@@ -18,8 +19,26 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
+		if (Ut.isNullOrEmpty(loginId)) {
+			return "아이디를 입력해주세요";
+		}
+		if (Ut.isNullOrEmpty(loginPw)) {
+			return "비밀번호를 입력해주세요";
+		}
+		if (Ut.isNullOrEmpty(name)) {
+			return "이름을 입력해주세요";
+		}
+		if (Ut.isNullOrEmpty(nickname)) {
+			return "닉네임을 입력해주세요";
+		}
+		if (Ut.isNullOrEmpty(cellphoneNum)) {
+			return "전화번호를 입력해주세요";
+		}
+		if (Ut.isNullOrEmpty(email)) {
+			return "이메일을 입력해주세요";
+		}
 
-		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
 		if (id == -1) {
 			return "이미 사용중인 아이디입니다";
